ff479af78010898e24b3c2160bfd34feafeb5a20
Alisina <alisinasoltani82@gmail.com>
2024-10-28 19:01:02 +0330

    feat!: created a constructor for all message classes

--- Messages.h
+++ Messages.h
@@ -5,22 +5,39 @@ using namespace std;
 
 #ifndef __MESSAGES__
     #define __MESSAGES__
-    class SimpleMessage: public BaseMessage
-    {
+    class SimpleMessage: public BaseMessage {
         private:
-        string msg;
+            string msg;
+
+        public:
+            SimpleMessage(string message_text): BaseMessage() {
+                this->msg = message_text;
+            }
     };
 
-    class PostMessage: public SimpleMessage
-    {
+    class PostMessage: public SimpleMessage {
         private:
-        string imgPath;
+            string imgPath;
+
+        public:
+            PostMessage(string message_text, string image_path): SimpleMessage(message_text) {
+                this->imgPath = image_path;
+            }
     };
 
-    class VoteMessage: public BaseMessage
-    {
+    class VoteMessage: public BaseMessage {
         private: 
-        string voteTitle;
-        string options[10];
+            string voteTitle;
+            string options[10];
+            bool is_quiz;
+        
+        public:
+            VoteMessage(string vote_title, string* options, bool is_quiz): BaseMessage() {
+                this->voteTitle = vote_title;
+                for (int i = 0; i < 10; i++) {
+                    this->options[i] = options[i];
+                }
+                this->is_quiz = is_quiz;
+            }
     };
 #endif
